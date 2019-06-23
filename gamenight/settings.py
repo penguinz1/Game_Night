b0e4352b838ebf910857a8d852c76e346da369f0
Jack Ning <jack_ning5@yahoo.com>
2019-06-23 17:01:52 -0400

    Made security fixes

--- gamenight/settings.py
+++ gamenight/settings.py
@@ -180,3 +180,15 @@ ADMIN_REORDER = (
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age = 500)
 DATABASES['default'].update(db_from_env)
+
+# security fixes
+# SECURE_HSTS_SECONDS = 31536000 
+SECURE_HSTS_SECONDS = 10
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
